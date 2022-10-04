c67e75f52498db2295b6593d93580968f5a08f03
e1b20006 <e1b20006@oit.ac.jp>
2022-10-04 16:22:35 +0900

    足し算を実行する処理をGETリクエスト(QueryParam)で実装

--- src/main/java/oit/is/sum/kaizi/sum/controller/Sample21Controller.java
+++ src/main/java/oit/is/sum/kaizi/sum/controller/Sample21Controller.java
@@ -32,6 +32,16 @@ public class Sample21Controller {
     // ModelMap型変数のmodelにtasuResult1という名前の変数で，tasuResultの値を登録する．
     // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
     model.addAttribute("tasuResult1", tasuResult);
-    return "sample21.html";  
+    return "sample21.html";
+  }
+
+  @GetMapping("/sample23")
+  public String sample23(@RequestParam Integer tasu1, @RequestParam Integer tasu2, ModelMap model) {
+    int tasuResult = tasu1 + tasu2;
+    model.addAttribute("tasuResult2", tasuResult);
+    // ModelMap型変数のmodelにtasuResult2という名前の変数で，tasuResultの値を登録する．
+    // ここで値を登録するとthymeleafが受け取り，htmlで処理することができるようになる
+    return "sample21.html";
+
   }
 }
